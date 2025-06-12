7554c857580098a9c1b0eacdc8d5c715e552d89b
Ridhanovita <novitaridha796@gmail.com>
2025-06-12 11:04:45 +0700

    validasi antrian kosong dan siapkan posisi awal untuk iterasi

--- Queues.cpp
+++ Queues.cpp
@@ -60,4 +60,17 @@ class Queues {
                 FRONT = FRONT + 1;
         }
     }
+
+    void display() {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+    }
 };
