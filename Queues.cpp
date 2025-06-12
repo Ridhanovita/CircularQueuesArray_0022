498e58fd9e39d9e7ee19f00fce287bbd18ffd946
Ridhanovita <novitaridha796@gmail.com>
2025-06-12 10:59:41 +0700

    Penanganan antrian kosong dan pergerakan REAR

--- Queues.cpp
+++ Queues.cpp
@@ -23,5 +23,19 @@ class Queues {
             cout << "\nQueue overflow\n";
             return;
         }
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            // Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max -1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
