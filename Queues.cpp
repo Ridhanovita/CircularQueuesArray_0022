187721b9c46869b5fb005e60c8059650dc597daf
Ridhanovita <novitaridha796@gmail.com>
2025-06-12 11:07:16 +0700

    Menampilkan elemen dengan wrap-around (sirkular)

--- Queues.cpp
+++ Queues.cpp
@@ -81,5 +81,21 @@ class Queues {
             }
             cout << endl;
         }
+        else {
+            // JIka FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max -1) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
